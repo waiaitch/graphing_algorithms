464718e58b2261d02137a1d3b9846a26dbb9f4aa
ChongLeh <85861224+ChongLeh@users.noreply.github.com>
2021-09-11 18:37:42 +0800

    Implemented minHeap
    a):
    -Changed to directed graph
    -Changed to array for priority queue
    b):
    -Implemented minHeap
    
    -Combined both function for comparison

--- src/graphing_algorithms/Graph.java
+++ src/graphing_algorithms/Graph.java
@@ -1,115 +1,155 @@
 package graphing_algorithms;
 
-import java.util.Vector;
-import java.util.LinkedList;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+
 
 public class Graph {
+	
+	public static int comp;
+	
+	public static int[] Dijkstras2(Graphs graph, int source) {
+        int [] nodesVisited = new int [graph.vertices];
+        Queue<Edge> edgeQueue = new PriorityQueue<>();
+        int [] shortestDistance = new int[graph.vertices];
+        Arrays.fill(shortestDistance, Integer.MAX_VALUE);
+        int [] pi = new int[graph.vertices];
+        // Start with the source vertex.
+        int currentNode = source;
+        shortestDistance[source] = 0;
+        nodesVisited[source] = 1;
+        
+        //adjacency matrix takes 
+        
+        do {
+        	LinkedList<Edge> list = graph.adjacencylist[currentNode];
+            for (int col = 0; col<list.size();col++) {
+                // Take all edges from this node. Skip self & 0-weight edges
+                if (list.get(col).weight == 0) {
+                    continue;
+                }
+                edgeQueue.add(new Edge(currentNode, list.get(col).destination, list.get(col).weight));
 
-	static class Edge {
-	 int source;
-	 int destination;
-	 int weight;
+                // Update distance for this destination if better than existing.
+                if (shortestDistance[list.get(col).destination] > shortestDistance[currentNode] + list.get(col).weight) 
+                {
+                	pi[list.get(col).destination] = currentNode;
+                    shortestDistance[list.get(col).destination] = shortestDistance[currentNode] + list.get(col).weight;
+                }
+                comp++;
+            }
+
+            //find cheapest edge to a not already visited destination.
+            //remove() operations with logE
+            Edge edge = edgeQueue.remove();
+            comp++;
+            while (nodesVisited[edge.destination] != 0) {
+                if (edgeQueue.isEmpty()) {
+                	for(int x=0;x<pi.length;x++) {
+                		System.out.println("Vertex " + x + " is connected from" + pi[x]);
+                	}
+                    return shortestDistance;
+                }
+                edge = edgeQueue.remove();
+                comp++;
+            }
+
+            // Now that you've reached this edge as a destination, record it.
+            currentNode = edge.destination;
+            nodesVisited[edge.destination] = 1;
+            comp++;
+        } while (!edgeQueue.isEmpty());
+
+        return nodesVisited;
+    }
+	
+	static Graphs randomGraph(int vertex) {
+    	int src, dest, weight;
+    	int edges = ThreadLocalRandom.current().nextInt(vertex, (vertex*(vertex-1)/2));
+    	//System.out.println("Random edges: " + edges);
+		Graphs graph = new Graphs(vertex);
+		for (int i = 0; i < edges; i++) {
+			src = ThreadLocalRandom.current().nextInt(0, vertex);
+			dest = ThreadLocalRandom.current().nextInt(1,vertex);
+			weight = ThreadLocalRandom.current().nextInt(0,10);
+			//System.out.println("Src: " + src + ",Dest: " + dest);
+			graph.addEdge(src, dest, weight);
+		}
+
+		return graph;
+	}
+		 
+	 public static void main(String arg[])
+	    {
+    	/*Graphs graph = new Graphs(5);
+    	graph.addEdge(0,1,4);
+	    graph.addEdge(0,2,3);
+	   	graph.addEdge(0,3,8);
+    	graph.addEdge(0,4,9);
+    	graph.addEdge(2,1,2);
+    	graph.addEdge(2,3,4);
+    	graph.addEdge(1,4,2);
+    	graph.printGraph();
+    	*/
+    	int src = 0;
+    	double avgTime = 0;
+    	int avgComp = 0;
+    	int sample = 1;
+    	
+    	for(int i = 0; i < sample; i++) {
+    		comp = 0;
+    		//play around with vertices value
+        	Graphs graph = randomGraph(5);
+        	graph.printGraph();
+        	
+        	double startTime = System.nanoTime();
+        	int[] d = Dijkstras2(graph, src);
+        	double stopTime = System.nanoTime();
+        	avgTime += (stopTime - startTime)/1000000;
+        	avgComp += comp;
+        	
+        	//System.out.println(Arrays.toString(Dijkstras(graph.matrix, src)));
+        	
+        	for(int j = 0; j < d.length; j++) {
+        		System.out.println(String.format("Shortest path from %s to %s is %s", src, j, d[j]));
+        	}
+    	}
+    	
+    	
+    	System.out.println("Average comparisons: " + avgComp/sample);
+    	System.out.println("Average computational time taken: " + avgTime/sample + "ms");
+	    }
 	 
-	 //Start of Creating Graph
-	 public Edge(int source, int destination, int weight) {
-		 this.source = source;
-		 this.destination = destination;
-		 this.weight = weight;
-	 	}
-	 }
-	
+}
 
-	 static class Graphs {
-		 int vertices;
-		 LinkedList<Edge> [] adjacencylist;
-	
-		 Graphs(int vertices) {
-			 this.vertices = vertices;
-			 adjacencylist = new LinkedList[vertices];
-			 //initialize adjacency lists for all the vertices
-			 for (int i = 0; i <vertices ; i++) {
-				 adjacencylist[i] = new LinkedList<>();
-			 }
+
+
+class Graphs {
+	int vertices;
+	LinkedList<Edge> [] adjacencylist;
+
+	@SuppressWarnings("unchecked")
+	Graphs(int vertices) {
+		this.vertices = vertices;
+		adjacencylist = new LinkedList[vertices];
+		//initialize adjacency lists for all the vertices
+		 for (int i = 0; i <vertices ; i++) {
+			 adjacencylist[i] = new LinkedList<>();
+		 }
 	 }
 
 	 public void addEdge(int source, int destination, int weight) {
 		 Edge edge = new Edge(source, destination, weight);
 		 adjacencylist[source].addFirst(edge); //for directed graph
 	 }
-
+	
 	 public void printGraph(){
 	 for (int i = 0; i <vertices ; i++) {
 		 LinkedList<Edge> list = adjacencylist[i];
 			 for (int j = 0; j <list.size() ; j++) {
 				 System.out.println("vertex-" + i + " is connected to " +
-				 list.get(j).destination + " with weight " + list.get(j).weight);
-				 }
+	 list.get(j).destination + " with weight " + list.get(j).weight);
 			 }
 		 }
 	 }
-	 //End of Creating Graph
-	 
-	 
-	 //Start of min Heap
-	 public static int[] dij(Graphs G, int source) {
-		 int vert = G.vertices;
-		 int d[] = new int[vert];
-		 int pi[] = new int[vert];
-		 int S[] = new int[vert];
-		 for(int x=0;x<vert;x++) {
-			 d[x] = 10000;
-			 pi[x] = -1;
-			 S[x] = 0;
-		 }
-		 d[source] = 0;
-		 int Q[] = new int[vert];
-		 for(int x=0;x<vert;x++) {
-			 Q[x] = x;
-		 }
-		 int empty = 0;
-		 while(empty!=vert) {
-			 int u = extractCheap(d,empty);
-			 S[u] = 1;
-			 LinkedList<Edge> list = G.adjacencylist[u];
-			 int numOfVertex = list.size();
-			 for(int v=0;v<numOfVertex;v++) {
-				 if(S[v]!=1 && d[v]>d[u]+list.get(v).weight) {
-					 Q[empty] = v;
-					 d[v] = d[u]+list.get(v).weight;
-					 pi[v] = u;
-				 }
-			 }
-			 empty++;
-		 }
-		 return Q; 
-	 }
-	 
-	 public static int extractCheap(int d[], int Q) {
-		 int min = 10001;
-		 for(int x=Q;x<d.length;x++) {
-			 if(d[x]<min) {
-				 min = d[x];
-			 }
-		 }
-		 return min;
-	 }
-	 
-	 
-	 
-	 public static void main(String[] args) {
-		 int vertices = 5;
-		 Graphs graph = new Graphs(vertices);
-		 graph.addEdge(0, 1, 4);
-		 graph.addEdge(0, 2, 3);
-		 graph.addEdge(1, 3, 2);
-		 graph.addEdge(1, 2, 5);
-		 graph.addEdge(2, 3, 7);
-		 graph.addEdge(3, 4, 2);
-		 graph.addEdge(4, 0, 4);
-		 graph.addEdge(4, 1, 4);
-		 graph.addEdge(4, 5, 6);
-		 graph.printGraph();
-		 int[] P = new int[vertices];
-		 P = dij(graph,0);
-	 }
-}
+ }
--- src/graphing_algorithms/Part_B.java
+++ src/graphing_algorithms/Part_B.java
@@ -1,58 +1,60 @@
 package graphing_algorithms;
 
-import java.util.Vector;
+import java.util.*;
+
 
 public class Part_B {
+
+	public class DirectedWeightedGraph<E> {
+
+		// Map having Vertex as key and List of Edges as Value.
+		Map<Vertex<E>, List<Edge<E>>> adj = new HashMap<>();
+
+
+		public static class Vertex<E> {
+		    E value;
+
+		    public Vertex(E value) {
+		        this.value = value;
+		    }
+		}
+
+		public static class Edge<E> {
+		    E from;
+		    E to;
+		    double weight;
+
+		    public Edge(E from, E to, double weight) {
+		        this.from = from;
+		        this.to = to;
+		        this.weight = weight;
+		    }
+
+		}
+
+		public void addVertex(E value) {
+		    Vertex<E> v = new Vertex<E>(value);
+		    List<Edge<E>> edges = new ArrayList<>();
+		    this.adj.put(v, edges);
+		}
+
+		public void addEdge(E from, E to, double weight) {
+		    List<Edge<E>> fromEdges =  this.;
+		    List<Edge<E>> toEdges =  this.getEdges(from);
+
+		    // Add source vertex and then add edge
+		    if(fromEdges == null) {
+		        this.addVertex(from);
+		    }
+		    if(toEdges == null) {
+		        this.addVertex(to);
+		    }
+
+		    fromEdges.add(new Edge<E>(from, to, weight));
+		}
 	
-	int V; // No. of vertices
-    Vector<Integer>[] adj; // No. of vertices
-
-    
-    // Constructor
-    @SuppressWarnings("unchecked")
-    Part_B(int V)
-    {
-        this.V = V;
-        this.adj = new Vector[2 * V];
-
-        for (int i = 0; i < 2 * V; i++)
-            this.adj[i] = new Vector<>();
+    public static void main (String[] args) {
+        
     }
-    
-	// adds an edge
-    public void addEdge(int v, int w, int weight)
-    {
-
-        // split all edges of weight 2 into two
-        // edges of weight 1 each. The intermediate
-        // vertex number is maximum vertex number + 1,
-        // that is V.
-        if (weight == 2) 
-        {
-            adj[v].add(v + this.V);
-            adj[v + this.V].add(w);
-        } else // Weight is 1
-            adj[v].add(w); // Add w to v's list.
-    }
-
-    
-    public static void main(String[] args)
-    {
-  
-        // Create a graph given in the above diagram
-        int V = 4;
-        Graph g = new Graph(V);
-        g.addEdge(0, 1, 2);
-        g.addEdge(0, 2, 2);
-        g.addEdge(1, 2, 1);
-        g.addEdge(1, 3, 1);
-        g.addEdge(2, 0, 1);
-        g.addEdge(2, 3, 2);
-        g.addEdge(3, 3, 2);
-  
-        int src = 0, dest = 3;
-        System.out.printf("\nShortest Distance between" + 
-                            " %d and %d is %d\n", src, 
-                            dest, g.findShortestPath(src, dest));
-    }
+	
 }
