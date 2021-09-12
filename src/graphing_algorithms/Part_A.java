6a9ae3f800beea40777a68de88dc9ea8ade572f3
waiaitch <kokyeehung@gmail.com>
2021-09-12 15:54:40 +0800

    Update randomGraph
    - every vertex is now connected to a random vertex

--- src/graphing_algorithms/Part_A.java
+++ src/graphing_algorithms/Part_A.java
@@ -5,10 +5,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Part_A {
 	
-	public static int comp;
 	public static GraphAdjacencyMatrix graph;
 	public static Graphs graph2;
-	public static int comp2;
 	
 
     public static int[] Dijkstras(int [][] graph, int source) {
@@ -40,7 +38,6 @@ public class Part_A {
                 	shortestDistance[col] = shortestDistance[currentNode] + graph[currentNode][col];
                 	insert(Q,col,shortestDistance[col]);
                 }
-                comp++;
             }
 
             //find cheapest edge to a not already visited destination.
@@ -52,7 +49,6 @@ public class Part_A {
             	setMaxToZero(shortestDistance);
             	return shortestDistance;
             }
-            comp++;
             while (nodesVisited[edge] != 0) {
                 if (Q[Q.length-1]==0) {
                 	for(int x=0;x<pi.length;x++) {
@@ -61,14 +57,12 @@ public class Part_A {
                     return shortestDistance;
                 }
                 edge = remove(Q);
-                comp++;
             }
 
             // Now that you've reached this edge as a destination, record it.
             currentNode = edge;
             nodesVisited[edge] = 1;
             Q[edge] = -1;
-            comp++;
         } while (Q[Q.length-1]!=0);
         
         setMaxToZero(shortestDistance);
@@ -126,7 +120,6 @@ public class Part_A {
                 	pi[list.get(col).destination] = currentNode;
                     shortestDistance[list.get(col).destination] = shortestDistance[currentNode] + list.get(col).weight;
                 }
-                comp2++;
             }
 
             //find cheapest edge to a not already visited destination.
@@ -140,7 +133,6 @@ public class Part_A {
             else {
             	edge = edgeQueue.remove();
             }
-            comp2++;
             while (nodesVisited[edge.destination] != 0) {
                 if (edgeQueue.isEmpty()) {
                 	for(int x=0;x<pi.length;x++) {
@@ -150,13 +142,11 @@ public class Part_A {
                     return shortestDistance;
                 }
                 edge = edgeQueue.remove();
-                comp2++;
             }
 
             // Now that you've reached this edge as a destination, record it.
             currentNode = edge.destination;
             nodesVisited[edge.destination] = 1;
-            comp2++;
         } while (!edgeQueue.isEmpty());
         
         setMaxToZero(shortestDistance);
@@ -167,31 +157,23 @@ public class Part_A {
     //Helper functions
     static GraphAdjacencyMatrix randomGraph(int vertex) {
     	int src, dest, weight;
-    	int edges = ThreadLocalRandom.current().nextInt(1, (vertex*(vertex-1)/2));
+    	//int maxEdge = (vertex*(vertex-1)/2);
+    	//play around with the edges value eg vertex, vertex/2, (vertex*(vertex-1)/2), 2
+    	int edges = 2000000;
     	boolean add;
 		graph = new GraphAdjacencyMatrix(vertex);
 		graph2 = new Graphs(vertex);
+		int v = 0;
 		for (int i = 0; i < edges; i++) {
 			add = true;
-			src = ThreadLocalRandom.current().nextInt(0, vertex);
+			if(v == vertex)
+				v = 0;
+			src = v;
 			dest = ThreadLocalRandom.current().nextInt(1,vertex);
-			do {
-				if(src != dest) {
-					break;
-				}
-				else {
-					src = ThreadLocalRandom.current().nextInt(0, vertex);
-					dest = ThreadLocalRandom.current().nextInt(1,vertex);
-				}
-			} while(src != dest);
-			weight = ThreadLocalRandom.current().nextInt(0,10);
-			if(graph.matrix[src][dest] != 0)
-				add = false;
-			if(add) {
+			weight = ThreadLocalRandom.current().nextInt(1,10);
 				graph.addEdge(src, dest, weight);
 				graph2.addEdge(src, dest, weight);
-			}
-			
+			v++;
 		}
 
 		return graph;
@@ -207,60 +189,42 @@ public class Part_A {
     // Main driver method
     public static void main(String arg[])
     {
-    	/*GraphAdjacencyMatrix graph = new GraphAdjacencyMatrix(5);
-    	graph.addEdge(0,1,4);
-	    graph.addEdge(0,2,7);
-	   	graph.addEdge(1,4,2);
-    	graph.addEdge(1,3,3);
-    	graph.addEdge(2,3,6);
-    	graph.addEdge(3,2,3);
-    	graph.addEdge(1,4,2);
-    	*/
     	int src = 0;
     	double avgTime = 0;
     	int avgComp = 0;
     	double avgTime2 = 0;
     	int avgComp2 = 0;
-    	int sample = 1;
+    	int sample = 10;
     	
     	for(int i = 0; i < sample; i++) {
-    		comp = 0;
-    		comp2 = 0;
     		//play around with vertices value
-        	randomGraph(1000);
-        	System.out.println();
-        	
-        	graph.printGraph();
-        	System.out.println();
-        	graph2.printGraph();
-        	System.out.println();
+        	randomGraph(10000);
+//        	System.out.println();
+//        	
+//        	graph.printGraph();
+//        	System.out.println();
+//        	graph2.printGraph();
+//        	System.out.println();
         	
         	double startTime = System.nanoTime();
         	int[] d = Dijkstras(graph.matrix, src);
         	double stopTime = System.nanoTime();
         	avgTime += (stopTime - startTime)/1000000;
-        	avgComp += comp;
         	double startTime2 = System.nanoTime();
         	int[] d2 = Dijkstras2(graph2, src);
         	double stopTime2 = System.nanoTime();
         	avgTime2 += (stopTime2 - startTime2)/1000000;
-        	avgComp2 += comp;
         	
-        	//System.out.println(Arrays.toString(Dijkstras(graph.matrix, src)));
-        	
-        	for(int j = 0; j < d.length; j++) {
-        		System.out.println(String.format("Shortest path from %s to %s is %s", src, j, d[j]));
-        	}
-        	System.out.println();
-        	for(int j = 0; j < d.length; j++) {
-        		System.out.println(String.format("Shortest path from %s to %s is %s", src, j, d2[j]));
-        	}
+//        	for(int j = 0; j < d.length; j++) {
+//        		System.out.println(String.format("Shortest path from %s to %s is %s", src, j, d[j]));
+//        	}
+//        	System.out.println();
+//        	for(int j = 0; j < d.length; j++) {
+//        		System.out.println(String.format("Shortest path from %s to %s is %s", src, j, d2[j]));
+//        	}
     	}
     	
-    	
-    	System.out.println("Average comparisons: " + avgComp/sample);
     	System.out.println("Average computational time taken: " + avgTime/sample + "ms");
-    	System.out.println("Average comparisons: " + avgComp2/sample);
     	System.out.println("Average computational time taken: " + avgTime2/sample + "ms");
     }
 }
@@ -318,6 +282,7 @@ class GraphAdjacencyMatrix {
 	public void addEdge(int source, int destination, int weight) {
 		// add edge
 		matrix[source][destination] = weight;
+		//matrix[destination][source] = weight;
 		edge++;
 	}
 
